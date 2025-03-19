28df9fa2e0aed478a4ca1c9e73c48899e2d9378f
Dang <nhda6k53>
2025-03-20 00:24:31 +0700

    minor changes

--- src/client/src/modules/admin/employeemanagement/employee-addedit/employee-addedit.component.ts
+++ src/client/src/modules/admin/employeemanagement/employee-addedit/employee-addedit.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import {
+  FontAwesomeModule,
+  IconDefinition,
+} from '@fortawesome/angular-fontawesome';
+import { faCamera } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-employee-addedit',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './employee-addedit.component.html',
-  styleUrl: './employee-addedit.component.css'
+  styleUrl: './employee-addedit.component.css',
 })
 export class EmployeeAddeditComponent {
-
+  //#region Font Awesome Icons
+  public faCamera: IconDefinition = faCamera;
+  //#endregion
 }
--- src/client/src/modules/admin/employeemanagement/employee-detail/employee-detail.component.ts
+++ src/client/src/modules/admin/employeemanagement/employee-detail/employee-detail.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule, IconDefinition } from '@fortawesome/angular-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-employee-detail',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './employee-detail.component.html',
-  styleUrl: './employee-detail.component.css'
+  styleUrl: './employee-detail.component.css',
 })
 export class EmployeeDetailComponent {
-
+  //#region Font Awesome Icons
+  public faUser: IconDefinition = faUser;
+  //#endregion
 }
