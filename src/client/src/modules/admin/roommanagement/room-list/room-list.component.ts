b95d9810c56a1987223cc08d3bf14282db2ff1e9
Dang <nhda6k53>
2025-03-19 13:01:38 +0700

    draft html room-list

--- src/client/src/modules/admin/roommanagement/room-list/room-list.component.ts
+++ src/client/src/modules/admin/roommanagement/room-list/room-list.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RoomDetailComponent } from '../room-detail/room-detail.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faAngleLeft,
+  faAngleRight,
+  faArrowLeft,
+  faInfoCircle,
+  IconDefinition,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-roommanagement',
-  imports: [RoomDetailComponent],
+  imports: [RoomDetailComponent, FontAwesomeModule],
   templateUrl: './room-list.component.html',
-  styleUrl: './room-list.component.css'
+  styleUrl: './room-list.component.css',
 })
 export class RoommanagementComponent {
-
+  //#region Font Awesome Icons
+  public faArrowLeft: IconDefinition = faArrowLeft;
+  public faInfoCircle: IconDefinition = faInfoCircle;
+  public faAngleRight: IconDefinition = faAngleRight;
+  public faAngleLeft: IconDefinition = faAngleLeft;
 }
