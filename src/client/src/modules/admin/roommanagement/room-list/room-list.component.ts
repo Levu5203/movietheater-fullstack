82f9b44ce9411703ad264c848aba1a091c8339cf
Dang <nhda6k53>
2025-03-21 01:57:47 +0700

    update room ui

--- src/client/src/modules/admin/roommanagement/room-list/room-list.component.ts
+++ src/client/src/modules/admin/roommanagement/room-list/room-list.component.ts
@@ -7,11 +7,17 @@ import {
   faArrowLeft,
   faInfoCircle,
   IconDefinition,
+  faFilter,
+  faRotateLeft,
+  faAnglesLeft,
+  faAnglesRight,
 } from '@fortawesome/free-solid-svg-icons';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-roommanagement',
-  imports: [RoomDetailComponent, FontAwesomeModule],
+  imports: [RoomDetailComponent, FontAwesomeModule, FormsModule, CommonModule],
   templateUrl: './room-list.component.html',
   styleUrl: './room-list.component.css',
 })
@@ -21,5 +27,17 @@ export class RoommanagementComponent {
   public faInfoCircle: IconDefinition = faInfoCircle;
   public faAngleRight: IconDefinition = faAngleRight;
   public faAngleLeft: IconDefinition = faAngleLeft;
+  public faFilter: IconDefinition = faFilter;
+  public faRotateLeft: IconDefinition = faRotateLeft;
+  public faAnglesLeft: IconDefinition = faAnglesLeft;
+  public faAnglesRight: IconDefinition = faAnglesRight;
   //#endregion
+
+  public isDropdownOpen: boolean = false;
+  public currentPage: number = 1;
+  public totalPages: number = 10;
+
+  public toggleDropdown(): void {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
 }
