5169e198aa09ddd7d83ef3f3f492be337bc8a225
Trieu Duy Thai <trieuduythai2003@gmail.com>
2025-03-22 14:33:08 +0700

    update sidebar core + Thai's modules

---
+++ src/client/src/modules/admin/addpromotion/addpromotion.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addpromotion',
+  imports: [],
+  templateUrl: './addpromotion.component.html',
+  styleUrl: './addpromotion.component.css'
+})
+export class AddpromotionComponent {
+
+}
