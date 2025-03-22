b0010e6d5349adceabe6ff280fa0e54f88b2a825
pvdu2003 <152840528+pvdu2003@users.noreply.github.com>
2025-03-22 16:19:38 +0700

    Add UI for customer list and update UI for edit profile

--- src/client/src/modules/customer/profile/edit-profile/edit-profile.component.ts
+++ src/client/src/modules/customer/profile/edit-profile/edit-profile.component.ts
@@ -1,14 +1,21 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  FontAwesomeModule,
+  IconDefinition,
+} from '@fortawesome/angular-fontawesome';
+import { faCalendar } from '@fortawesome/free-solid-svg-icons';
 import { ModalService } from '../../../../services/modal.service';
 import { ChangePasswordComponent } from '../change-password/change-password.component';
 
 @Component({
   selector: 'app-edit-profile',
-  imports: [ChangePasswordComponent],
+  imports: [FontAwesomeModule, ChangePasswordComponent],
   templateUrl: './edit-profile.component.html',
   styleUrl: './edit-profile.component.css',
 })
 export class EditProfileComponent {
+  public faCalendar: IconDefinition = faCalendar;
+
   constructor(private modalService: ModalService) {}
   public isShowModal: boolean = false;
   openModal() {
