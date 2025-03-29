497f50652f057403f5d123c1d71f97d1f31d66fc
Dang <nhda6k53>
2025-03-29 21:58:25 +0700

    BE

--- src/server/MovieTheater.Business/Handlers/Profile/ChangePasswordCommand.cs
+++ src/server/MovieTheater.Business/Handlers/Profile/ChangePasswordCommand.cs
@@ -7,5 +7,4 @@ public class ChangePasswordCommand : IRequest<bool>
     public required Guid UserId { get; set; }
     public required string CurrentPassword { get; set; }
     public required string NewPassword { get; set; }
-    public required string ConfirmPassword { get; set; }
 }
