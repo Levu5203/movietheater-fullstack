3f0128491dcbf1e0038ddb15baabd3517aac4532
Levu5023 <levi01237797458@gmail.com>
2025-03-30 22:43:56 +0700

    Create Api Showtime

--- src/server/MovieTheater.Business/Mappings/MappingProfile.cs
+++ src/server/MovieTheater.Business/Mappings/MappingProfile.cs
@@ -13,6 +13,7 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         CreateMap<LoginRequestCommand, LoginResponse>();
+        //
         CreateMap<Movie, MovieViewModel>()
             .ForMember(dest => dest.Genres,
                 opt => opt.MapFrom(src => src.MovieGenres.Select(mg => mg.Genre.Type).ToList()))
@@ -23,16 +24,12 @@ public class MappingProfile : Profile
                     .ToList()))
             .ForMember(dest => dest.Showtimes,
                 opt => opt.MapFrom(src => src.ShowTimes));
+        //Mapper for showtime
         CreateMap<ShowTime, ShowtimeViewModel>()
+            .ForMember(dest => dest.MovieName, opt => opt.MapFrom(src => src.Movie.Name))
             .ForMember(dest => dest.RoomName, 
                 opt => opt.MapFrom(src => src.CinemaRoom.Name))
             .ForMember(dest => dest.StartTime, 
                opt => opt.MapFrom(src => src.ShowTimeSlot.Time.ToString(@"hh\:mm\:ss")));
-        // public Guid MovieId { get; set; }
-        // public Guid RoomId { get; set; }
-        // public Guid ShowtimeSlotId { get; set; }
-        // public DateOnly ShowDate { get; set; }
-        // public string? MovieName { get; set; }
-        // public string? RoomName { get; set; }
     }
 }
--- src/server/MovieTheater.WebAPI/Controllers/ShowtimeController.cs
+++ src/server/MovieTheater.WebAPI/Controllers/ShowtimeController.cs
@@ -1,8 +1,38 @@
 using System;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MovieTheater.Business.Handlers.Showtime;
+using MovieTheater.Business.ViewModels.Showtime;
 
-namespace MovieTheater.WebAPI.Controllers;
-
-public class ShowtimeController
+namespace MovieTheater.WebAPI.Controllers
 {
+    /// <summary>
+    /// Authen API Controller 
+    /// </summary>
+    /// <param name="mediator"></param>
+    [Route("api/[controller]")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    [ApiExplorerSettings(GroupName = "v1")]
 
+    public class ShowtimeController(IMediator mediator) : ControllerBase
+    {
+        private readonly IMediator _mediator = mediator;
+        [HttpGet("showtimes")]
+        [ProducesResponseType(typeof(IEnumerable<ShowtimeViewModel>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var query = new ShowtimeGetAllQuery();
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(ShowtimeViewModel), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetByIdAsync(Guid id)
+        {
+            var query = new ShowtimeGetByIdQuery{Id = id};
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+    }
 }
