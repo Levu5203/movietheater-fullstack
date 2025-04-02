18b1d8b4bdc31723c7b44b486ddc185ce1139378
Levu5023 <levi01237797458@gmail.com>
2025-04-02 11:08:17 +0700

    Add StatusInvoice

---
+++ src/server/MovieTheater.Data/Migrations/20250402040706_AddStatusInvoice.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieTheater.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusInvoice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                schema: "Common",
+                table: "Invoices",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                schema: "Common",
+                table: "Invoices");
+        }
+    }
+}
