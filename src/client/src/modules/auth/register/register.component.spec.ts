7a636d78735c48728715f174a868824e326df4e3
pvdu2003 <152840528+pvdu2003@users.noreply.github.com>
2025-04-07 15:19:21 +0700

    Refactor authentication-related tests: update component names, improve test setup with MockAuthService, and enhance module imports for better test coverage.

--- src/client/src/modules/auth/register/register.component.spec.ts
+++ src/client/src/modules/auth/register/register.component.spec.ts
@@ -1,16 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
+import { MockAuthService } from '../../../../testing/mock-service';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AUTH_SERVICE } from '../../../constants/injection.constant';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let mockAuthService: MockAuthService;
 
   beforeEach(async () => {
+    mockAuthService = new MockAuthService();
+
     await TestBed.configureTestingModule({
-      imports: [RegisterComponent]
-    })
-    .compileComponents();
+      imports: [
+        RegisterComponent,
+        ReactiveFormsModule,
+        CommonModule,
+        FontAwesomeModule,
+      ],
+      providers: [{ provide: AUTH_SERVICE, useValue: mockAuthService }],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
