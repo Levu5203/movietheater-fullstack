ffd2c92c8dd8f0a7be964c82edf21f23c965a904
Levu5023 <levi01237797458@gmail.com>
2025-04-17 01:15:50 +0700

    pagination promotion

--- src/client/src/modules/customer/promotion/promotion.component.ts
+++ src/client/src/modules/customer/promotion/promotion.component.ts
@@ -12,6 +12,8 @@ import { RouterLink } from '@angular/router';
 })
 export class PromotionComponent implements OnInit {
   public promotions: PromotionModel[] = [];
+  public currentPage: number = 1;
+  public itemsPerPage: number = 8;
   constructor(private http: HttpClient) {}
   ngOnInit(): void {
     this.fetchPromotions();
@@ -24,5 +26,24 @@ export class PromotionComponent implements OnInit {
         console.log('Promotion data:', this.promotions);
       });
   }
+  get paginatedPromotions(): PromotionModel[] {
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    return this.promotions.slice(start, start + this.itemsPerPage);
+  }
 
+  get totalPages(): number {
+    return Math.ceil(this.promotions.length / this.itemsPerPage);
+  }
+
+  goNext() {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+    }
+  }
+
+  goBack() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
 }
