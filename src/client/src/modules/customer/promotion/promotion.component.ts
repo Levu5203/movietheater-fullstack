ec2b7cb3a8190b2cb157f21e5a4ef01648368594
Levu5023 <levi01237797458@gmail.com>
2025-04-17 01:01:49 +0700

    add promotion detail

--- src/client/src/modules/customer/promotion/promotion.component.ts
+++ src/client/src/modules/customer/promotion/promotion.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PromotionModel } from '../../../models/promotion/promotion.model';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-promotion',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './promotion.component.html',
   styleUrl: './promotion.component.css'
 })
-export class PromotionComponent {
+export class PromotionComponent implements OnInit {
+  public promotions: PromotionModel[] = [];
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.fetchPromotions();
+  }
+  private fetchPromotions() {
+    this.http
+      .get<PromotionModel[]>(`http://localhost:5063/api/Promotion`)
+      .subscribe((response: PromotionModel[]) => {
+        this.promotions = response;
+        console.log('Promotion data:', this.promotions);
+      });
+  }
 
 }
