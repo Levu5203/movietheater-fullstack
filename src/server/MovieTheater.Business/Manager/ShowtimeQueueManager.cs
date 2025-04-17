ab9555efe84dbfc2a74820faa91f83f513f9939c
Levu5023 <levi01237797458@gmail.com>
2025-04-18 00:25:49 +0700

    add request reserve to queue

---
+++ src/server/MovieTheater.Business/Manager/ShowtimeQueueManager.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Concurrent;
+
+namespace MovieTheater.Business.Manager;
+
+public class ShowtimeQueueManager
+{
+    private readonly ConcurrentDictionary<Guid, SemaphoreSlim> _semaphoreByShowtime = new();
+
+    public async Task<T> EnqueueAsync<T>(Guid showtimeId, Func<Task<T>> work)
+    {
+        var semaphore = _semaphoreByShowtime.GetOrAdd(showtimeId, _ => new SemaphoreSlim(1, 1));
+        await semaphore.WaitAsync();
+
+        try
+        {
+            return await work();
+        }
+        finally
+        {
+            semaphore.Release();
+        }
+    }
+}
